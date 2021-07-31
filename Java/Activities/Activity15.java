719accb32d73ef9b9bd34ec7b3715ae5a6761aa3
slnarasimhan108 <88019102+slnarasimhan108@users.noreply.github.com>
2021-07-31 11:29:18 +0530

    Add files via upload

---
+++ Java/Activities/Activity15.java
@@ -0,0 +1,15 @@
+package activities;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Activity15 {
+	
+	private static String FILE_NAME = "testSheet.xlsx";
+
+	public static void main(String[] args) {
+		
+		XSSFWorkbook workbook = new XSSFworkbook();
+	}
+
+}
